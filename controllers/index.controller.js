8801d0a71bcad4d636028b09ed48fd437580fd3d
richard101088 <113070613+richard101088@users.noreply.github.com>
2022-10-25 17:01:33 -0400

    v.1

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -2,21 +2,21 @@
 
 exports.home = function(req, res, next) {
   console.log('===> Original URL: ' + req.session.url);
-  res.render('index', { 
+  res.render('Home', { 
       title: 'Home',
       userName: req.user ? req.user.username : ''
   });
 };
 
 exports.about = function(req, res, next) {
-  res.render('index', { 
+  res.render('about', { 
       title: 'About',
       userName: req.user ? req.user.username : ''
    });
 }
 
 exports.projects = function(req, res, next) {
-  res.render('index', { 
+  res.render('projects', { 
     title: 'Projects',
     userName: req.user ? req.user.username : '' 
   });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-let controlerIndex = require('../controllers/index');
+let controlerIndex = require('../controllers/index.controller');
 
 /* GET home page. */
 router.get('/', controlerIndex.home);
